c818258ecef115a396e0984295ec130265235a66
Ash1# <fortuneengineer@gmail.com>
2021-03-12 21:05:16 +0530

    new component add

--- src/app/events/event.thumbnail.component.ts
+++ src/app/events/event.thumbnail.component.ts
@@ -3,16 +3,32 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector:'event-thumbnail',
     template:` <div class="well hoverwell thumbnail">
-    <h2>{{event1.name}}</h2>
-    <div>Date: {{event1.date}}</div>
-    <div>Time: {{event1.time}}</div>
-    <div>Price: {{event1.price}}</div>
-    <div>
+    <h2>{{event1?.name}}</h2>
+    <div>Date: {{event1?.date}}</div>
+    <div [ngSwitch] ="event1?.time">
+        Time: {{event1?.time}}
+    <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+    <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+    <span *ngSwitchDefault>(Normal Start)</span>
+    </div>
+    <div>Price: {{event1?.price}}</div>
+    <div *ngIf="event1.location">
         <span>Location: {{event1.location.address}}</span>
         <span>&nbsp;</span>
         <span>{{event1.location.city}}, {{event1.location.country}}</span>
     </div>
-</div>`
+    <div *ngIf = "event1.onlineUrl">
+        Online Url: {{event1.onlineUrl}}
+    </div>
+</div>`,
+styles:[`.hoverwell{background-color:rgba(255,255,255, 0.6)}
+.well{
+    height:200px;
+}
+.hoverwell:hover {
+    background-color:rgba(255,255,255, 0.8);
+    cursor: pointer;
+  }`]
 })
 
 export class EventThumbnailComponent{
